5492e9614dee980f4d62367b0b500cf3f2034750
Mlopezsantorum <mlopezsantorum@danielcastelao.org>
2021-11-08 09:54:07 +0100

    añado comprobar

--- app/src/main/java/com/example/simondice/MainActivity.kt
+++ app/src/main/java/com/example/simondice/MainActivity.kt
@@ -14,37 +14,69 @@ import kotlinx.coroutines.*
 class MainActivity : AppCompatActivity() {
     var contadorRonda = 1
     override fun onCreate(savedInstanceState: Bundle?) {
-            super.onCreate(savedInstanceState)
-            setContentView(R.layout.activity_main)
-            var game = ArrayList<Int>()
-            var jugador = ArrayList<Int>()
-            val bot: Button = findViewById(R.id.botonjugar)
-            val empezar = findViewById<Button>(R.id.botonjugar)
-            var finalizado = false
-            val comprobar = findViewById<Button>(R.id.comprobar)
-            val verde = findViewById<Button>(R.id.BOTONVERDE)
-            val azul = findViewById<Button>(R.id.BOTONAZUL)
-            val amarillo = findViewById<Button>(R.id.BOTONAMARILLO)
-            val rojo = findViewById<Button>(R.id.BOTONROJO)
-            val listaBotones = listOf(verde, azul, amarillo, rojo)
-            val toast = Toast.makeText(applicationContext, "A llorar con mama", Toast.LENGTH_SHORT)
-            val toast3 = Toast.makeText(applicationContext, "Ya sabes que hacer", Toast.LENGTH_SHORT)
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        var game = ArrayList<Int>()
+        var jugador = ArrayList<Int>()
+        val bot: Button = findViewById(R.id.botonjugar)
+        val empezar = findViewById<Button>(R.id.botonjugar)
+        var finalizado = false
+        val comprobar = findViewById<Button>(R.id.comprobar)
+        val verde = findViewById<Button>(R.id.BOTONVERDE)
+        val azul = findViewById<Button>(R.id.BOTONAZUL)
+        val amarillo = findViewById<Button>(R.id.BOTONAMARILLO)
+        val rojo = findViewById<Button>(R.id.BOTONROJO)
+        val listaBotones = listOf(verde, azul, amarillo, rojo)
+        val toast = Toast.makeText(applicationContext, "A llorar con mama", Toast.LENGTH_SHORT)
+        val toast3 = Toast.makeText(applicationContext, "Ya sabes que hacer", Toast.LENGTH_SHORT)
 
+        empezar.setOnClickListener {
+            finalizado = false
+            reset(game, jugador)
+            añadirSecuencia(game)
+            ejecutarSecuencia(game, listaBotones)
+            toast3.show()
+            mostrarRonda()
+            bot.visibility = View.INVISIBLE
+            Log.d("Estado", "Jugar")
+        }
 
-
-
-
-
-
-    private fun Empezar() {
-        Log.i("Estado", "Empieza la partida")
-        val mostrarRonda: TextView = findViewById(R.id.cuadrotexto)
-        mostrarRonda.text = contadorRonda.toString()
-
-        botonjugar.setOnClickListener {
-
+        comprobar.setOnClickListener {
+            Log.d("Estado", "Comprobar secuencia")
+            contadorRonda++
+            if (finalizado == false) {
+                if (checkSecuencia(game, jugador)) {
+                    Log.d("Estado", "Ronda acertada")
+                    añadirSecuencia(game)
+                    jugador.clear()
+                    ejecutarSecuencia(game, listaBotones)
+                    mostrarRonda()
+                } else {
+                    finalizado = true
+                    toast.show()
+                    contadorRonda = 0
+                    val bot: Button = findViewById(R.id.botonjugar)
+                    bot.visibility = View.VISIBLE
+                    Log.d("Estado", "GAME OVER")
+                }
+            }
 
         }
+
+        verde.setOnClickListener {
+            añadirSecuenciaUsuario(jugador, 1)
+        }
+        azul.setOnClickListener {
+            añadirSecuenciaUsuario(jugador, 2)
+        }
+        amarillo.setOnClickListener {
+            añadirSecuenciaUsuario(jugador, 3)
+        }
+        rojo.setOnClickListener {
+            añadirSecuenciaUsuario(jugador, 4)
+        }
+
+
 
 
 
@@ -99,3 +131,4 @@ class MainActivity : AppCompatActivity() {
         }
     }
 }
+
